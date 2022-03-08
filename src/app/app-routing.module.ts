5ab7a5f10183fe3b7d493648e6ecc3171450dbc8
Jodecir Neto <jodecirneto@gmail.com>
2022-03-08 10:34:15 -0300

    CREATE pokemon list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'folder/pokemon-list',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'pokemon-list',
+    loadChildren: () => import('./pages/Pokemon/pokemon-list/pokemon-list/pokemon-list.module').then( m => m.PokemonListPageModule)
   }
 ];
 
